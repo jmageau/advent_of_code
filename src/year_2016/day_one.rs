a3a47330114ad6382d90492456b688bb9d09393a
Julien Mageau <jmage019@uottawa.ca>
2016-12-01 21:51:47 -0500

    Changed input to be read from file

--- src/year_2016/day_one.rs
+++ src/year_2016/day_one.rs
@@ -1,4 +1,5 @@
-const INPUT: &'static str = "R3, R1, R4, L4, R3, R1, R1, L3, L5, L5, L3, R1, R4, L2, L1, R3, L3, R2, R1, R1, L5, L2, L1, R2, L4, R1, L2, L4, R2, R2, L2, L4, L3, R1, R4, R3, L1, R1, L5, R4, L2, R185, L2, R4, R49, L3, L4, R5, R1, R1, L1, L1, R2, L1, L4, R4, R5, R4, L3, L5, R1, R71, L1, R1, R186, L5, L2, R5, R4, R1, L5, L2, R3, R2, R5, R5, R4, R1, R4, R2, L1, R4, L1, L4, L5, L4, R4, R5, R1, L2, L4, L1, L5, L3, L5, R2, L5, R4, L4, R3, R3, R1, R4, L1, L2, R2, L1, R4, R2, R2, R5, R2, R5, L1, R1, L4, R5, R4, R2, R4, L5, R3, R2, R5, R3, L3, L5, L4, L3, L2, L2, R3, R2, L1, L1, L5, R1, L3, R3, R4, R5, L3, L5, R1, L3, L5, L5, L2, R1, L3, L1, L3, R4, L1, R3, L2, L2, R3, R3, R4, R4, R1, L4, R1, L5";
+use std::io::prelude::*;
+use std::fs::File;
 
 pub fn answers() -> String {
     format!("{}, {}", answer_one(), answer_two())
@@ -13,7 +14,8 @@ fn answer_two() -> String {
 }
 
 fn distance(stop_at_visited: bool) -> String {
-    let instructions = INPUT.split(", ").collect::<Vec<_>>();
+    let input = input();
+    let instructions = input.split(", ").collect::<Vec<_>>();
     let directions = vec![(0, 1), (1, 0), (0, -1), (-1, 0)];
 
     let mut direction = 0;
@@ -44,4 +46,11 @@ fn distance(stop_at_visited: bool) -> String {
     }
 
         (position.0.abs() + position.1.abs()).to_string()
+}
+
+fn input() -> String {
+    let mut file = File::open("src/year_2016/input/input_day_one").unwrap();
+    let mut string = String::new();
+    let _ = file.read_to_string(&mut string);
+    string
 }
