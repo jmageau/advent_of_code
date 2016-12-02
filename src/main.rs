e6209bd49c744da5331ede30ef20c188933a2c13
Julien Mageau <jmage019@uottawa.ca>
2016-12-01 21:32:53 -0500

    Added solution for 2015 day one

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+mod year_2015;
 mod year_2016;
 
 fn main() {
+    year_2015::print_all_answers();
     year_2016::print_all_answers();
 }
--- src/year_2016/day_one.rs
+++ src/year_2016/day_one.rs
@@ -1,3 +1,5 @@
+const INPUT: &'static str = "R3, R1, R4, L4, R3, R1, R1, L3, L5, L5, L3, R1, R4, L2, L1, R3, L3, R2, R1, R1, L5, L2, L1, R2, L4, R1, L2, L4, R2, R2, L2, L4, L3, R1, R4, R3, L1, R1, L5, R4, L2, R185, L2, R4, R49, L3, L4, R5, R1, R1, L1, L1, R2, L1, L4, R4, R5, R4, L3, L5, R1, R71, L1, R1, R186, L5, L2, R5, R4, R1, L5, L2, R3, R2, R5, R5, R4, R1, R4, R2, L1, R4, L1, L4, L5, L4, R4, R5, R1, L2, L4, L1, L5, L3, L5, R2, L5, R4, L4, R3, R3, R1, R4, L1, L2, R2, L1, R4, R2, R2, R5, R2, R5, L1, R1, L4, R5, R4, R2, R4, L5, R3, R2, R5, R3, L3, L5, L4, L3, L2, L2, R3, R2, L1, L1, L5, R1, L3, R3, R4, R5, L3, L5, R1, L3, L5, L5, L2, R1, L3, L1, L3, R4, L1, R3, L2, L2, R3, R3, R4, R4, R1, L4, R1, L5";
+
 pub fn answers() -> String {
     format!("{}, {}", answer_one(), answer_two())
 }
@@ -11,8 +13,7 @@ fn answer_two() -> String {
 }
 
 fn distance(stop_at_visited: bool) -> String {
-    let input = "R3, R1, R4, L4, R3, R1, R1, L3, L5, L5, L3, R1, R4, L2, L1, R3, L3, R2, R1, R1, L5, L2, L1, R2, L4, R1, L2, L4, R2, R2, L2, L4, L3, R1, R4, R3, L1, R1, L5, R4, L2, R185, L2, R4, R49, L3, L4, R5, R1, R1, L1, L1, R2, L1, L4, R4, R5, R4, L3, L5, R1, R71, L1, R1, R186, L5, L2, R5, R4, R1, L5, L2, R3, R2, R5, R5, R4, R1, R4, R2, L1, R4, L1, L4, L5, L4, R4, R5, R1, L2, L4, L1, L5, L3, L5, R2, L5, R4, L4, R3, R3, R1, R4, L1, L2, R2, L1, R4, R2, R2, R5, R2, R5, L1, R1, L4, R5, R4, R2, R4, L5, R3, R2, R5, R3, L3, L5, L4, L3, L2, L2, R3, R2, L1, L1, L5, R1, L3, R3, R4, R5, L3, L5, R1, L3, L5, L5, L2, R1, L3, L1, L3, R4, L1, R3, L2, L2, R3, R3, R4, R4, R1, L4, R1, L5";
-    let instructions = input.split(", ").collect::<Vec<_>>();
+    let instructions = INPUT.split(", ").collect::<Vec<_>>();
     let directions = vec![(0, 1), (1, 0), (0, -1), (-1, 0)];
 
     let mut direction = 0;
