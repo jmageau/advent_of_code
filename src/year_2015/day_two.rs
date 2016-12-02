ffff81bcb2f3f27e3b558edfd717205488381acc
Julien Mageau <jmage019@uottawa.ca>
2016-12-01 23:27:02 -0500

    Added solution for 2015 day two

---
+++ src/year_2015/day_two.rs
@@ -0,0 +1,41 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+pub fn answers() -> String {
+    format!("{}, {}", answer_one(), answer_two())
+}
+
+fn answer_one() -> String {
+    let input = input();
+    let lines = input.lines();
+
+    let mut total_area = 0;
+    for line in lines {
+        let dimensions: Vec<u32> = line.split('x').map(|d| d.parse::<u32>().unwrap()).collect();
+        let smallest_side_area = dimensions.iter().product::<u32>() / dimensions.iter().max().unwrap();
+        let area = 2 * (dimensions[0] * dimensions[1] + dimensions[1] * dimensions[2] + dimensions[2] * dimensions[0]) + smallest_side_area;
+        total_area += area;
+    }
+    total_area.to_string()
+}
+
+fn answer_two() -> String {
+    let input = input();
+    let lines = input.lines();
+
+    let mut total_length = 0;
+    for line in lines {
+        let dimensions: Vec<u32> = line.split('x').map(|d| d.parse::<u32>().unwrap()).collect();
+        let wrap_length = 2 * (dimensions.iter().sum::<u32>() - dimensions.iter().max().unwrap());
+        let ribbon_length = dimensions.iter().product::<u32>();
+        total_length += wrap_length + ribbon_length;
+    }
+    total_length.to_string()
+}
+
+fn input() -> String {
+    let mut file = File::open("src/year_2015/input/input_day_two").unwrap();
+    let mut string = String::new();
+    let _ = file.read_to_string(&mut string);
+    string
+}
--- src/year_2015/mod.rs
+++ src/year_2015/mod.rs
@@ -1,7 +1,9 @@
 mod day_one;
+mod day_two;
 
 pub fn print_all_answers() {
     println!("2015");
     println!("1. {}", day_one::answers());
+    println!("2. {}", day_two::answers());
     println!("");
 }
