95f4dc47bd3eb357230cbf5cf44e642a61939f70
jmageau <julien.mageau@live.ca>
2017-12-02 13:40:07 -0500

    Added solution for 2017 day 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-#![feature(ordering_chaining )]
-
 mod year_2015;
 mod year_2016;
+mod year_2017;
 
 fn main() {
-//    year_2016::print_all_answers();
-    year_2015::print_all_answers();
+    // year_2015::print_all_answers();
+    // year_2016::print_all_answers();
+    year_2017::print_all_answers();
 }
---
+++ src/year_2017/day_1.rs
@@ -0,0 +1,35 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+
+pub fn answers() -> String {
+    format!("{}, {}", answer_one(), answer_two())
+}
+
+fn answer_one() -> String {
+    let input = input();
+    let digits = input.chars().filter_map(|c| c.to_digit(10)).collect();
+    sum(digits, 1).to_string()
+}
+
+fn answer_two() -> String {
+    let input = input();
+    let digits = input.chars().filter_map(|c| c.to_digit(10)).collect();
+    sum(digits, input.len() / 2).to_string()
+}
+
+fn sum(digits: Vec<u32>, offset: usize) -> u32 {
+    digits
+        .iter()
+        .enumerate()
+        .filter(|&(i, &d)| d == digits[(i + offset) % digits.len()])
+        .map(|(_, d)| d)
+        .sum()
+}
+
+fn input() -> String {
+    let mut file = File::open("src/year_2017/input/input_day_1").unwrap();
+    let mut string = String::new();
+    let _ = file.read_to_string(&mut string);
+    string.trim().to_owned()
+}
