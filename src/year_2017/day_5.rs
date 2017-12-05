93464e82f0940eb83924d5b61052ffcc6fb25da6
jmageau <julien.mageau@live.ca>
2017-12-05 14:09:04 -0500

    Added solution for 2017 day 5

---
+++ src/year_2017/day_5.rs
@@ -0,0 +1,48 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+pub fn answers() -> String {
+    format!("{}, {}", answer_one(), answer_two())
+}
+
+fn answer_one() -> String {
+    let input = input();
+    let mut instructions: Vec<i32> = input.lines().map(|n| n.parse().unwrap()).collect();
+    let mut current_index = 0i32;
+    let mut steps = 0;
+
+    while current_index >= 0 && current_index < instructions.len() as i32 {
+        instructions[current_index as usize] += 1;
+        current_index += instructions[current_index as usize] - 1;
+        steps += 1;
+    }
+
+    steps.to_string()
+}
+
+fn answer_two() -> String {
+    let input = input();
+    let mut instructions: Vec<i32> = input.lines().map(|n| n.parse().unwrap()).collect();
+    let mut current_index = 0i32;
+    let mut steps = 0;
+
+    while current_index >= 0 && current_index < instructions.len() as i32 {
+        let change = if instructions[current_index as usize] >= 3 {
+            -1
+        } else {
+            1
+        };
+        instructions[current_index as usize] += change;
+        current_index += instructions[current_index as usize] - change;
+        steps += 1;
+    }
+
+    steps.to_string()
+}
+
+fn input() -> String {
+    let mut file = File::open("src/year_2017/input/input_day_5").unwrap();
+    let mut string = String::new();
+    let _ = file.read_to_string(&mut string);
+    string.trim().to_owned()
+}
